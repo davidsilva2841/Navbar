31a2f5a6cda87305df93fae7f1297e43559cf9e7
ds <davidsilva2841+accounts+gitthub@gmail.com>
2019-12-03 12:44:43 -0600

    Bootstrap checkpoint

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import {Navbar, Nav, Form, FormControl} from 'react-bootstrap';
+import {Navbar, Nav, Form, FormControl, NavDropdown} from 'react-bootstrap';
 
 
 const NavBar = () => {
     return (
         <div id="navbar-container">
 
-            <Navbar.Brand href="/" id="navbar-logo">
+            <Navbar.Brand href="/" id="navbar-logo" className="ml-4">
                 <h2
                     style={{
                         color: 'white',
-                        fontWeight: 'bold'
+                        fontFamily: 'Passion One',
+                        fontWeight: '700',
+                        margin: '10px 0 0 0'
                     }}
                 >
-                    WEST BUY
+                    <span>WEST</span>
+                    <br/>
+                    <span>BUY</span>
+                    <img
+                        alt=""
+                        width="60"
+                        height="60"
+                        src="logo.svg"
+                    />
                 </h2>
-                <img
-                    alt=""
-                    width="60"
-                    height="60"
-                    src="logo.gif"
-                />
+
             </Navbar.Brand>
 
             <div id="navbar">
@@ -37,11 +42,25 @@ const NavBar = () => {
                     </Navbar>
                 </Navbar>
 
+
                 <Navbar>
                     <Form inline>
                         <FormControl type="text" placeholder="Search West Buy" className="mr-sm-2" />
                     </Form>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/localstore">Austin, TX </Nav.Link>
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                    </Nav>
                 </Navbar>
+
+                <Navbar>
+                    <Nav>
+                        <NavDropdown title="Products">
+
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
+
             </div>
         </div>
     )
